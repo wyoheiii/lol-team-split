5145d42055b2fb3780cbd8d66c3db7e76266afe1
wyo <seikimatu4649@gmail.com>
2025-11-06 02:56:58 +0900

    add print

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod param;
 mod pipeline;
 mod splitter;
 mod assigner;
-
+mod print;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/print/console.rs
@@ -0,0 +1,18 @@
+use crate::domain::Player;
+use crate::pipeline::states::AssignedTeams;
+use crate::domain::types::{RoleMap, Side};
+
+pub fn print_teams(teams: &AssignedTeams) {
+  let p = |t :&RoleMap<Player>, side: Side| {
+    println!("========= {} SIDE =========", side);
+    println!("{:<12}  {:<4}  {}", "Player", "Role", "Rank");
+    println!("-------------------------------------");
+    for (p, r) in t.iter() {
+      println!("{:<12}  {:<4}  {}", p.name, r, p.rank);
+    }
+    println!();
+  };
+
+  p(&teams.red, Side::Red);
+  p(&teams.blue, Side::Blue);
+}
