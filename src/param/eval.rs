685f5fef356caf6119043a3c45afb5c05d748a80
wyo <seikimatu4649@gmail.com>
2025-11-11 13:52:16 +0900

    add eval context

--- src/param/eval.rs
+++ src/param/eval.rs
@@ -1,4 +1,4 @@
-use crate::{param::mmr, domain::states::Lobby};
+use crate::{domain::states::Lobby, param::{mmr::{self, MMR}, penalty_matrix::PenaltyMatrix, role_weights::RoleWeights}};
 
 
 
@@ -21,18 +21,53 @@ impl Default for Eval {
 
 #[derive(Debug, Clone)]
 pub struct LobbyStats {
+  // 平均
   pub mean: f64,
+  // 標準偏差
   pub std: f64,
 }
 
 impl LobbyStats {
-pub fn from_players(players: &Lobby) -> Self {
-let mut vals: Vec<f64> = players.players().iter().map(|p| abs_mmr_from_rank(p.rank, &RatingModelCfg::default())).collect();
-if vals.is_empty() { return Self{ mean:0.0, std:1.0 }; }
-let n = vals.len() as f64;
-let mean = vals.iter().sum::<f64>() / n;
-let var = vals.iter().map(|v| (v - mean).powi(2)).sum::<f64>() / n;
-let std = var.sqrt();
-Self { mean, std: if std==0.0 { 1.0 } else { std } }
+pub fn from_lobby(lobby: &Lobby, mmr: &MMR) -> Self {
+    let mut vals: Vec<f64> = lobby.players().iter().map(|p| mmr.calculate(&p.rank)).collect();
+    if vals.is_empty() {
+      return Self{ mean:0.0, std:1.0 };
+    }
+
+    let n = vals.len() as f64;
+    let mean = vals.iter().sum::<f64>() / n;
+    let var = vals.iter().map(|v| (v - mean).powi(2)).sum::<f64>() / n;
+    let std = var.sqrt();
+    Self { mean, std: if std==0.0 { 1.0 } else { std } }
+  }
+
+  // プレイヤーの相対的な位置(z score)を算出
+  pub fn z_from(&self,  base: f64) -> f64 {
+    (base - self.mean) / self.std
+  }
 }
+
+// zスコアからペナルティ倍率を算出
+pub fn penalty_multiplier_from_z(z: f64, alpha: f64) -> f64 {
+  (1.0 + (-z) * alpha).clamp(0.6, 1.8)
+}
+
+
+#[derive(Debug, Clone)]
+pub struct EvalContext {
+  pub penalty: PenaltyMatrix,
+  pub role_weight: RoleWeights,
+  pub eval: Eval,
+  pub lobby: LobbyStats,
+}
+
+impl EvalContext {
+  pub fn new(lobby: &Lobby)->Self {
+    Self {
+      penalty: PenaltyMatrix::default(),
+      role_weight: RoleWeights::default(),
+      eval: Eval::default(),
+      lobby: LobbyStats::from_lobby(lobby, &MMR::default()),
+    }
+  }
 }
