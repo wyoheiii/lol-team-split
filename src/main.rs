a1ff321ef35861179da4748b40c4e5ad879615f7
wyo <seikimatu4649@gmail.com>
2025-11-14 19:21:48 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use crate::param::eval::EvalContext;
+use crate::splitter::role_snake::RoleSnakeSplitter;
 use crate::{domain::Rank, domain::states::Lobby};
 use crate::pipeline::TeamSolver;
 use crate::splitter::random::RandomSplitter;
 use crate::assigner::random::RandomRoleAssigner;
+use crate::assigner::brute_force::BruteForceAssigner;
 use crate::pipeline::DefaultSolver;
 use print::console::print_teams;
 use demo_lobby::sample_lobbies;
@@ -22,8 +25,14 @@ fn main() {
 
   for (id, lobby) in sample_lobbies() {
     println!("=== Solving Lobby: {} ===", id);
-    let splitter = RandomSplitter::new(42);
-    let assigner = RandomRoleAssigner::new(42);
+    for p in lobby.players() {
+      println!("  - {:<12} (Role: {} ,Rank: {})", p.name, p.main_role, p.rank);
+    }
+    // let splitter = RandomSplitter::new(42);
+    // let assigner = RandomRoleAssigner::new(42);
+    let eval = EvalContext::new(&lobby);
+    let splitter = RoleSnakeSplitter::new(eval.eval.mmr.clone());
+    let assigner = BruteForceAssigner::new(eval);
     let solver   = DefaultSolver::new(splitter, assigner);
     print_teams(&solver.solve(&lobby));
   }
